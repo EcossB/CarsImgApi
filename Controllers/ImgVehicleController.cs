34dad5e9ab0b9ba6432b8b512611713c480ad577
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-13 16:31:02 -0400

    Creating  controller to handle the calls to the service.

---
+++ Controllers/ImgVehicleController.cs
@@ -0,0 +1,27 @@
+﻿using CarsImgApi.Interface;
+using CarsImgApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsImgApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImgVehicleController : ControllerBase
+    {
+        private readonly IImageVehicle _interfaceImg;
+
+        public ImgVehicleController(IImageVehicle interfaceImg)
+        {
+            _interfaceImg = interfaceImg;   
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> saveImgData(ImgVehicleModel imgVehicle)
+        {
+            _interfaceImg.addImagesVehicle(imgVehicle);
+            return Ok("Imagenes Del vehiculo Guardados!");
+        }
+
+    }
+}
---
+++ Controllers/VehicleController.cs
@@ -0,0 +1,44 @@
+﻿using CarsImgApi.Interface;
+using CarsImgApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsImgApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+
+        private readonly IDatosVehiculos _interfaceVehicles;
+
+
+        public VehicleController(IDatosVehiculos interfaceVehicles)
+        {
+            _interfaceVehicles = interfaceVehicles;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ModeloVehiculo>>> GetAllVehiclesData()
+        {
+            var vehicles = _interfaceVehicles.getAllVehiclesData();
+            return Ok(vehicles);
+        }
+
+        [HttpGet("{chasis}")]
+
+        public async Task<ActionResult<ModeloVehiculo>> getVehicleByChasis(string chasis)
+        {
+            var vehicle = _interfaceVehicles.getVehicleByChasis(chasis);
+            return Ok(vehicle);
+        }
+
+        [HttpGet("/chasis")]
+        public async Task<ActionResult<List<ChasisModel>>> getAllChasis()
+        {
+            var chasis = _interfaceVehicles.getAllChasis();
+            return Ok(chasis);
+        }
+
+    }
+}
