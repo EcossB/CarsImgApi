bb5eac6953077c85e61fdcb62a1bea65cac99454
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-13 16:28:41 -0400

    Model vehicle to send the model to the frontend of the view retrieved

---
+++ Models/ModeloVehiculo.cs
@@ -0,0 +1,16 @@
+﻿namespace CarsImgApi.Models
+{
+    public class ModeloVehiculo
+    {
+        public string? Compania { get; set; } = string.Empty;
+        public string? Sucursal {  get; set; } = string.Empty;
+        public int Orden_Numero { get; set; } = 0;
+        public string? Chasis { get; set; } = string.Empty;
+        public int Marca { get; set; } = 0;
+        public int Modelo { get; set; } = 0;
+        public int Ano { get; set; } = 0;
+        public string? Placa { get; set; } = string.Empty;
+        public int Color { get; set; } = 0;
+        
+    }
+}
