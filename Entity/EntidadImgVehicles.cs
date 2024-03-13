26b65555011e464b4fecc65513612ff89cb529a6
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-13 16:31:29 -0400

    Entity img vehicle data

---
+++ Entity/EntidadImgVehicles.cs
@@ -0,0 +1,14 @@
+﻿namespace CarsImgApi.Entity
+{
+    public class EntidadImgVehicles
+    {
+        public string? Compania { get; set; } = string.Empty;
+        public string? Sucursal { get; set; } = string.Empty;
+        public int Orden_Numero { get; set; } = 0;
+        public string? Img_lateral_derecho { get; set; } = string.Empty;
+        public string? Img_lateral_izquierdo { get; set; } = string.Empty;
+        public string? Img_frontal { get; set; } = string.Empty;
+        public string? Img_trasero { get; set; } = string.Empty;
+
+    }
+}
