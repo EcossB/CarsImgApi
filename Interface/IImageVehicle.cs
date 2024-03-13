3b49ee6248ae18cc6d4710fa3d72b81588a00df2
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-13 17:24:10 -0400

    Methods to be implemented on the service.

--- Interface/IImageVehicle.cs
+++ Interface/IImageVehicle.cs
@@ -5,5 +5,9 @@ namespace CarsImgApi.Interface
     public interface IImageVehicle
     {
         void addImagesVehicle(ImgVehicleModel vehicle);
+
+        List<ImgVehicleModel> getAllImagesVehicles();
+
+        ImgVehicleModel getImageVehicle(int num_order);
     }
 }
