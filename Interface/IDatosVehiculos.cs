95db2cdc5f7d035732bab7718d7ecbe3ba66d6ad
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-13 16:30:34 -0400

    Creating interface For Principle I of SOLID

---
+++ Interface/IDatosVehiculos.cs
@@ -0,0 +1,13 @@
+﻿using CarsImgApi.Models;
+
+namespace CarsImgApi.Interface
+{
+    public interface IDatosVehiculos
+    {
+        List<ModeloVehiculo> getAllVehiclesData();
+
+        ModeloVehiculo getVehicleByChasis(string chasis);
+
+        List<ChasisModel> getAllChasis();
+    }
+}
