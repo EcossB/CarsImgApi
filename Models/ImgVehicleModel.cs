f8e6238a2e7ba79ec8260e7bb5e9a00cc6269936
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-13 16:29:10 -0400

    This model is sent to the client to see the data of the images.

---
+++ Models/ImgVehicleModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CarsImgApi.Models
+{
+    public class ImgVehicleModel
+    {
+        public string? Compania { get; set; } = string.Empty;
+        public string? Sucursal { get; set; } = string.Empty;
+        public int Orden_Numero { get; set; } = 0;
+        public string? Img_lateral_derecho { get; set; } = string.Empty;
+        public string? Img_lateral_izquierdo { get; set; } = string.Empty;
+        public string? Img_frontal { get; set; } = string.Empty;
+        public string? Img_trasero { get; set; } = string.Empty;
+    }
+}
