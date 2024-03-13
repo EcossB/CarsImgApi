e78ccd3c1f29e77c1fd87eaa421655a43a607ae5
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-13 16:27:09 -0400

    adding scope for service injection

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using CarsImgApi.Interface;
+using CarsImgApi.services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IDatosVehiculos, DataVehicleService>();
+builder.Services.AddScoped<IImageVehicle, ImgVehiclesService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
