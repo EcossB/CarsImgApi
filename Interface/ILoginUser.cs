f2f7cfec380f818fe902ef7d5522214903fb8945
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-20 17:25:15 -0400

    new interface, this one is injected in the login controller.

---
+++ Interface/ILoginUser.cs
@@ -0,0 +1,12 @@
+﻿using CarsImgApi.Entity;
+using CarsImgApi.Models;
+
+namespace CarsImgApi.Interface
+{
+    public interface ILoginUser
+    {
+        public LoginModel login(UserSqlConnection user);
+
+        public void logOut(UserSqlConnection user);
+    }
+}
