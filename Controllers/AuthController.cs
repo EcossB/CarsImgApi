fcd92d406f9200b1bfe02d5ddd88ee5f23b5b302
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-20 17:23:01 -0400

    Creating new controller to manage login

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using CarsImgApi.Entity;
+using CarsImgApi.Interface;
+using CarsImgApi.Models;
+using CarsImgApi.services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsImgApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILoginUser _authService;
+
+        public AuthController(ILoginUser service)
+        {
+            _authService = service;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<LoginModel>> login(UserSqlConnection user)
+        {
+            var token = _authService.login(user);
+            if (token.token.Length > 40)
+            {
+                return Ok(token);
+            }
+            return BadRequest(token);
+        }
+
+        [HttpPost("/logout")]
+        public async Task<ActionResult<string>> LogOut(UserSqlConnection token)
+        {
+            _authService.logOut(token);
+            return Ok("Log out Succesfully");
+        }
+
+
+
+
+    }
+}
