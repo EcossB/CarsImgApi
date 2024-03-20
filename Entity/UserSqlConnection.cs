69af5d735426258089b22f7927853b74d894431f
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-20 17:23:56 -0400

    UserSqlConnection

---
+++ Entity/UserSqlConnection.cs
@@ -0,0 +1,10 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace CarsImgApi.Entity
+{
+    public class UserSqlConnection
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
