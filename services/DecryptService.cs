e8cace6031c7d0381d15a10ccdeccd4cfc256172
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-20 17:28:35 -0400

    Creating a decrypt service to decrypt the token the user send in the header

---
+++ services/DecryptService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace CarsImgApi.services
+{
+    public class DecryptService
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public DecryptService(IConfiguration configuration)
+        {
+
+            _configuration = configuration; 
+
+        }
+
+
+        /*This method take the token and extract the name. */
+        public string GetName(string token)
+        {
+            string secret = _configuration.GetSection("AppSettings:Token").Value;
+            var key = Encoding.ASCII.GetBytes(secret);
+            var handler = new JwtSecurityTokenHandler();
+            var validations = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+            var claims = handler.ValidateToken(token, validations, out var tokenSecure);
+            return claims.Identity.Name;
+        }
+    }
+}
