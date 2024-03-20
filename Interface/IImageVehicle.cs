590c38a3cbc14b25833dd7f1e6a858a15a14d873
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-20 17:25:00 -0400

    new interface, this one is injected in the image controller.

--- Interface/IImageVehicle.cs
+++ Interface/IImageVehicle.cs
@@ -4,10 +4,11 @@ namespace CarsImgApi.Interface
 {
     public interface IImageVehicle
     {
-        void addImagesVehicle(ImgVehicleModel vehicle);
+        void addImagesVehicle(ImgVehicleModel vehicle, string user);
 
-        List<ImgVehicleModel> getAllImagesVehicles();
+        List<ImgVehicleModel> getAllImagesVehicles(string user);
 
-        ImgVehicleModel getImageVehicle(int num_order);
+        ImgVehicleModel getImageVehicle(int num_order, string user);
+
     }
 }
