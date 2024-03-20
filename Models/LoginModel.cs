ac72c90b50fc108dfbc69ca127e840ebf8139908
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-20 17:25:46 -0400

    The model for login endpoint response

---
+++ Models/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarsImgApi.Models
+{
+    public class LoginModel
+    {
+        public string userName { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+
+        public string token { get; set; } = string.Empty;
+    }
+}
