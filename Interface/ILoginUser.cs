d2399228cabcfc27541eab2072d576f4ccd56902
Erwing Coss <erwingcoss21@hotmail.com>
2024-03-21 17:18:19 -0400

    changing return type

--- Interface/ILoginUser.cs
+++ Interface/ILoginUser.cs
@@ -7,6 +7,6 @@ namespace CarsImgApi.Interface
     {
         public LoginModel login(UserSqlConnection user);
 
-        public void logOut(UserSqlConnection user);
+        public MessageModel logOut(UserSqlConnection user);
     }
 }
