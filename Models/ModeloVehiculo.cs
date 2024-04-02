a0872a3697d6a52fac1c28af7531cb9594da0609
Erwing Coss <erwingcoss21@hotmail.com>
2024-04-02 15:17:47 -0400

    changing properties on the model.

--- Models/ModeloVehiculo.cs
+++ Models/ModeloVehiculo.cs
@@ -1,16 +1,15 @@
 ﻿namespace CarsImgApi.Models
 {
-    public class ModeloVehiculo
+    public class ModeloVehiculoRecepcion
     {
         public string? Compania { get; set; } = string.Empty;
         public string? Sucursal {  get; set; } = string.Empty;
         public int Orden_Numero { get; set; } = 0;
-        public string? Chasis { get; set; } = string.Empty;
-        public int Marca { get; set; } = 0;
-        public int Modelo { get; set; } = 0;
-        public int Ano { get; set; } = 0;
+        public DateTime Fecha_orden { get; set; }
+        public string? Nombre_cliente { get; set; } = string.Empty;
+        public string? Marca { get; set; } = string.Empty;
+        public string? Modelo { get; set; } = string.Empty;
         public string? Placa { get; set; } = string.Empty;
-        public int Color { get; set; } = 0;
         
     }
 }
