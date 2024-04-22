10fdd5ff54193d5dd8b14b04b8ab2a70714a710b
EcossB <erwingcoss21@hotmail.com>
2024-04-22 07:41:38 -0700

    adding new connectin string to the db

--- Entity/PoolSqlConnections.cs
+++ Entity/PoolSqlConnections.cs
@@ -45,7 +45,8 @@ namespace CarsImgApi.Entity
 
         public string getConnectionString(UserSqlConnection _user)
         {
-            return $"User Id={_user.userName.ToUpper()}; Password={_user.password}; Data Source=(DESCRIPTION =(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST = 127.0.0.1)(PORT = 1521))) (CONNECT_DATA =(SERVICE_NAME = xe)))";
+            return $"User Id={_user.userName}; Password={_user.password}; Data Source=(DESCRIPTION =(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST = 10.0.0.240)(PORT = 1521))) (CONNECT_DATA =(SERVICE_NAME = autodata)))";
+            //return $"User Id={_user.userName.ToUpper()}; Password={_user.password}; Data Source=(DESCRIPTION =(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST = 127.0.0.1)(PORT = 1521))) (CONNECT_DATA =(SERVICE_NAME = xe)))";
         }
 
     }
