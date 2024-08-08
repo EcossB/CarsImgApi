2174c882d8a0d3b2f2047fef4fe1922361b1f2da
Erwing Coss <erwingcoss21@hotmail.com>
2024-08-08 16:22:06 -0400

    changing return type.

--- Controllers/ImgVehicleController.cs
+++ Controllers/ImgVehicleController.cs
@@ -1,5 +1,5 @@
-﻿using CarsImgApi.Interface;
-using CarsImgApi.Models;
+﻿using CarsImgApi.Models.DTO.ImgVehicleDTOS;
+using CarsImgApi.Repository.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,7 +20,7 @@ namespace CarsImgApi.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<string>> saveImgData(ImgVehicleModel imgVehicle, string user)
+        public async Task<ActionResult<string>> saveImgData(ImgVehicleRequestDTO imgVehicle, string user)
         {
             if (imgVehicle.Img_frontal.Length > 0 &&
                 imgVehicle.Img_lateral_izquierdo.Length > 0 &&
@@ -36,38 +36,38 @@ namespace CarsImgApi.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<ImgVehicleModel>>> getAllVehiclesImg(string user)
+        public async Task<ActionResult<List<ImgVehicleRequestDTO>>> getAllVehiclesImg(string user)
         {
-            var vehicleList = _interfaceImg.getAllImagesVehicles(user);
+            var vehicleList = _interfaceImg.getAllImagesVehicles(user).Result;
             return Ok(vehicleList);
         }
 
         [HttpGet("{num_order}")]
-        public async Task<ActionResult<ImgVehicleModel>> getImgVehicleByNumOrder(int num_order, string user)
+        public async Task<ActionResult<ImgVehicleRequestDTO>> getImgVehicleByNumOrder(int num_order, string user)
         {
             var vehicleImg = _interfaceImg.getImageVehicle(num_order, user);
-            if(vehicleImg.Img_lateral_izquierdo.Length > 0)
+            if(vehicleImg.Result != null)
             {
-                return Ok(vehicleImg);
+                return Ok(vehicleImg.Result);
             }
             return BadRequest("No existe ese numero de orden.");
             
         }
 
         [HttpGet("/api/pagination")]
-        public async Task<ActionResult<List<ImgVehicleModel>>> imgPagination(string user, int pagina, int limiteRegistro)
+        public async Task<ActionResult<List<ImgVehicleRequestDTO>>> imgPagination(string user, int pagina, int limiteRegistro)
         {
             var vehicleImg = _interfaceImg.getNext(user, pagina, limiteRegistro);
 
-            return Ok(vehicleImg);
+            return Ok(vehicleImg.Result);
 
         }
 
         [HttpGet("/api/get4first")]
-        public async Task<ActionResult<List<ImgVehicleModel>>> getFirst5(string user)
+        public async Task<ActionResult<List<ImgVehicleRequestDTO>>> getFirst5(string user)
         {
             var vehicleImg = _interfaceImg.getFirst4(user);
-            return Ok(vehicleImg);
+            return Ok(vehicleImg.Result);
 
         }
 
