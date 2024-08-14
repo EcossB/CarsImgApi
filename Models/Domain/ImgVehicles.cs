b59f1bac005ca499c75cf56e6cf48e62da121231
Erwing Coss <erwingcoss21@hotmail.com>
2024-08-14 10:32:44 -0400

    refactoring imgvehicle domaing.

--- Models/Domain/ImgVehicles.cs
+++ Models/Domain/ImgVehicles.cs
@@ -9,6 +9,9 @@
         public string? Img_lateral_izquierdo { get; set; } = string.Empty;
         public string? Img_frontal { get; set; } = string.Empty;
         public string? Img_trasero { get; set; } = string.Empty;
+        public string? Img_anexo1 { get; set; } = string.Empty;
+        public string? Img_anexo2 { get; set; } = string.Empty;
+        public string? Img_anexo3 { get; set; } = string.Empty;
 
     }
 }
