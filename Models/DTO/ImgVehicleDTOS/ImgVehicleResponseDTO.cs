52fb67a40c1943b2c223b0c19f73a28b91489702
Erwing Coss <erwingcoss21@hotmail.com>
2024-08-14 11:01:29 -0400

    vehicle image response when retrieven data.

---
+++ Models/DTO/ImgVehicleDTOS/ImgVehicleResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace CarsImgApi.Models.DTO.ImgVehicleDTOS
+{
+    public class ImgVehicleResponseDTO
+    {
+        public string? Compania { get; set; } = string.Empty;
+        public string? Sucursal { get; set; } = string.Empty;
+        public int Orden_Numero { get; set; } = 0;
+        public string? Img_lateral_derecho { get; set; } = string.Empty;
+        public string? Img_lateral_izquierdo { get; set; } = string.Empty;
+        public string? Img_frontal { get; set; } = string.Empty;
+        public string? Img_trasero { get; set; } = string.Empty;
+        public string? Img_anexo1 { get; set; } = string.Empty;
+        public string? Img_anexo2 { get; set; } = string.Empty;
+        public string? Img_anexo3 { get; set; } = string.Empty;
+
+    }
+}
