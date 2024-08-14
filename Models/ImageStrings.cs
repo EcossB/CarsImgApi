a06f09cba834952739c7c1157504eddf944f0f41
Erwing Coss <erwingcoss21@hotmail.com>
2024-08-14 11:02:13 -0400

    A class to create a list of this type.

---
+++ Models/ImageStrings.cs
@@ -0,0 +1,13 @@
+﻿namespace CarsImgApi.Models
+{
+    public class ImageStrings
+    {
+        public string? Img_lateral_derecho { get; set; } = string.Empty;
+        public string? Img_lateral_izquierdo { get; set; } = string.Empty;
+        public string? Img_frontal { get; set; } = string.Empty;
+        public string? Img_trasero { get; set; } = string.Empty;
+        public string? Img_anexo1 { get; set; } = string.Empty;
+        public string? Img_anexo2 { get; set; } = string.Empty;
+        public string? Img_anexo3 { get; set; } = string.Empty;
+    }
+}
