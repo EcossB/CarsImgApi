95806caaf27bde932976731d21a4c2710a849864
EcossB <erwingcoss21@hotmail.com>
2025-02-20 14:03:06 -0400

    adding more parameter to TokenValidationParameters

--- Program.cs
+++ Program.cs
@@ -16,13 +16,15 @@ builder.Services.AddScoped<IDataVehicle, VehicleRepository>();
 builder.Services.AddScoped<IImageVehicle, VehicleImageRepository>();
 builder.Services.AddScoped<ILoginUser, AuthService>();
 builder.Services.AddScoped<ICreateImage, CreateImageService>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
     options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
     {
-        Description = "(\bearer {token})",
+        Description = "(\bBearer {token})",
         In = ParameterLocation.Header,
         Name = "Authorization",
         Type = SecuritySchemeType.ApiKey
@@ -32,17 +34,20 @@ builder.Services.AddSwaggerGen(options =>
 
 });
 
-
+//creando la autenticacion de la api
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
         options.TokenValidationParameters = new TokenValidationParameters
         {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-                builder.Configuration.GetSection("AppSettings:Token").Value)),
-            ValidateIssuer = false,
-            ValidateAudience = false
+            AuthenticationType = "Jwt", //aqui le decimos del tipoque es
+            ValidateIssuer = true, //aqui le decimos que valide quien firma el token que somos nosotros
+            ValidateAudience = true, // aqui validamos a la persona que va, que en este caso es el frontend
+            ValidateLifetime = true, //aqui le ponemos que valide si el token expiro por el tiempo
+            ValidateIssuerSigningKey = true, // aqui que valide la llave definida en el appsettings
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
         };
     });
 
