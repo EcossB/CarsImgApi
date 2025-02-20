f7e763e3c0dfbf284d7ab9ec6790dcb4566d83bd
EcossB <erwingcoss21@hotmail.com>
2025-02-20 14:02:18 -0400

    changing connection string

--- services/PoolSqlConnections.cs
+++ services/PoolSqlConnections.cs
@@ -46,9 +46,11 @@ namespace CarsImgApi.services
 
         public string getConnectionString(UserSqlConnection _user)
         {
-            return $"User Id={_user.userName}; Password={_user.password}; Data Source=(DESCRIPTION =(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST = localhost)(PORT = 1521))) (CONNECT_DATA =(SERVICE_NAME = pdb2)))";
+            // return $"User Id={_user.userName}; Password={_user.password}; Data Source=(DESCRIPTION =(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST = localhost)(PORT = 1521))) (CONNECT_DATA =(SERVICE_NAME = pdb2)))";
             //return $"User Id={_user.userName}; Password={_user.password}; Data Source=(DESCRIPTION =(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST = 127.0.0.1)(PORT = 1521))) (CONNECT_DATA =(SERVICE_NAME = xe)))";
             //return $"User Id={_user.userName}; Password={_user.password}; Data Source=(DESCRIPTION =(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST = 10.0.0.240)(PORT = 1521))) (CONNECT_DATA =(SERVICE_NAME = autodata)))";
+            return $"User Id={_user.userName}; Password={_user.password}; Data Source=(DESCRIPTION =(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST = localhost)(PORT = 1521))) (CONNECT_DATA =(SERVICE_NAME = ORCLPDB1)))";
+
         }
 
     }
