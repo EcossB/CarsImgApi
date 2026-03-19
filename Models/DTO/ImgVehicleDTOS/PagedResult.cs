48a1a0ad8b0b596a2c14d6212047b430f36e6614
EcossV <ecoss@grupoviamar.com>
2026-03-19 17:46:46 -0400

    Adding new dto for paginated data

---
+++ Models/DTO/ImgVehicleDTOS/PagedResult.cs
@@ -0,0 +1,12 @@
+﻿namespace CarsImgApi.Models.DTO.ImgVehicleDTOS
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T>? Data { get; set; } // Los 10 vehículos
+        public int TotalRecords { get; set; }    // Ej: 1540 (Total en la BD)
+        public int PageNumber { get; set; }      // Ej: 1
+        public int PageSize { get; set; }        // Ej: 10
+        public int TotalPages => (int) Math.Ceiling((double)TotalRecords / PageSize);
+        
+    }
+}
