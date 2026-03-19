fd5c5cff6359b8c5977a95bfa0d63caad44d2706
EcossV <ecoss@grupoviamar.com>
2026-03-19 17:47:49 -0400

    Updating signature method interface

--- Repository/Interface/ICreateImage.cs
+++ Repository/Interface/ICreateImage.cs
@@ -6,7 +6,7 @@ namespace CarsImgApi.Repository.Interface
     {
         public Task<List<string>> CreateImageAsync(ImgVehicles vehicleImages);
 
-        public Task<ImgVehicles> GetImageAsync(ImgVehicles vehicleImages);
+        public Task<ImgVehicles> GetImageAsync(ImgVehicles? vehicleImages);
 
         public Task<string> ReadAsbase64(string path);
     }
