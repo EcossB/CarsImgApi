bd0b7a749abdd7ecb8ac62a18447592cd6d75562
EcossV <ecoss@grupoviamar.com>
2026-03-19 17:47:32 -0400

    Adding nullable condition

--- Repository/Interface/IImageVehicle.cs
+++ Repository/Interface/IImageVehicle.cs
@@ -5,17 +5,15 @@ namespace CarsImgApi.Repository.Interface
 {
     public interface IImageVehicle
     {
-        public Task<ImgVehicles> AddImagesVehicle(ImgVehicles vehicle, string user);
+        public Task<ImgVehicles?> AddImagesVehicle(ImgVehicles vehicle, string? user);
 
         public Task<IEnumerable<ImgVehicles>> GetAllImagesVehicles(string user);
 
         public Task<ImgVehicles> GetImageVehicle(int num_order);
 
-        public Task<List<ImgVehicles>> PaginateImages(string user, int pagina, int limiteRegistro);
+        public Task<PagedResult<ImgVehicles>> PaginateImages(string user, int pagina, int limiteRegistro);
 
         public Task<IEnumerable<ImgVehicles>> Get4FirstImages(string user);
-
-        public Task<int> NumberPages(string user);
 
     }
 }
