b27af7816964af995b88ff754c562c13df3f28cd
EcossB <erwingcoss21@hotmail.com>
2026-03-22 23:19:19 -0400

    added new method in the interface contract

--- Repository/Interface/ICreateImage.cs
+++ Repository/Interface/ICreateImage.cs
@@ -1,11 +1,14 @@
 ﻿using CarsImgApi.Models.Domain;
+using CarsImgApi.Models.DTO.ImgVehicleDTOS;
 
 namespace CarsImgApi.Repository.Interface
 {
     public interface ICreateImage
     {
         public Task<List<string>> CreateImageAsync(ImgVehicles vehicleImages);
-
+        
+        public Task<string> CreateSingleImageAsync(IFormFile file, ImgSingleVehicleRequest img, string lado);
+        
         public Task<ImgVehicles> GetImageAsync(ImgVehicles? vehicleImages);
 
         public Task<string> ReadAsbase64(string path);
